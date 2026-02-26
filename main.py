c2e520da27c386d579f0aa83b480810995a7b2ef
temirkuliyevu-jpg <temirkuliyevu@gmail.com>
2026-02-26 11:06:16 +0500

    Initialize Flask app with basic routes

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# templates
+# static
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+# app.run(debug=True, host="127.0.0.1")
